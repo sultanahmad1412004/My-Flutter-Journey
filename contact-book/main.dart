283c8bd74e51105114b63ef3e841313b12f2012e
sultanahmad1412004 <factbites.1@gmail.com>
2025-04-03 19:12:07 +0500

    Create main.dart
    this is main file for my contact book layout in flutter

---
+++ contact-book/main.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:prac/show.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(length: 2, child: Home()),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final Map contacts = {"names": [], "phone": [], "address": []};
+  final name = TextEditingController();
+  final phone_no = TextEditingController();
+  final addres = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Contact Book", style: TextStyle(color: Colors.white)),
+          leading: Icon(Icons.menu_rounded, color: Colors.white, size: 30),
+          actions: [
+            Icon(Icons.add_circle, size: 30, color: Color(0xFFD6E5E3)),
+            SizedBox(width: 20),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Icon(
+                Icons.person_add_rounded,
+                size: 30,
+                color: Color(0xFFD6E5E3),
+              ),
+              Icon(Icons.list_rounded, size: 30, color: Color(0xFFD6E5E3)),
+            ],
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorWeight: 3.0,
+            indicatorColor: Color(0xFFCACFD6),
+          ),
+          backgroundColor: Color(0xFFff0030),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Color(0xFFff0030),
+                  radius: 42,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+                    ),
+                    radius: 40,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  width: 300,
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: name,
+                        decoration: InputDecoration(
+                          labelText: "Name",
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Color(0xFFff0030),
+                          ),
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        keyboardType: TextInputType.phone,
+                        controller: phone_no,
+                        decoration: InputDecoration(
+                          labelText: "Phone Number",
+                          prefixIcon: Icon(
+                            Icons.call,
+                            color: Color(0xFFff0030),
+                          ),
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        controller: addres,
+                        decoration: InputDecoration(
+                          labelText: "Address",
+                          prefixIcon: Icon(
+                            Icons.location_pin,
+                            color: Color(0xFFff0030),
+                          ),
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                      SizedBox(height: 15,),
+                      ElevatedButton(
+                        onPressed: () {
+                          FocusScope.of(context).unfocus();
+
+                          setState(() {
+                            if(name.text.isNotEmpty && phone_no.text.isNotEmpty && addres.text.isNotEmpty) {
+                              contacts["names"]?.add(name.text);
+                              contacts["phone"]?.add(phone_no.text);
+                              contacts["address"]?.add(addres.text);
+
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text("Number Saved Successfully!"),
+                                  margin: EdgeInsets.only(
+                                    bottom: MediaQuery.of(context).size.height - 200,
+                                    left: 10,
+                                    right: 10,
+                                  ),
+                                  behavior: SnackBarBehavior.floating,
+                                  dismissDirection: DismissDirection.up,
+                                  backgroundColor: Colors.green,
+                                  showCloseIcon: true,
+                                  closeIconColor: Colors.red,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                              );
+
+                              phone_no.clear();
+                              addres.clear();
+                              name.clear();
+                            }else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Row(
+                                    children: [
+                                      Icon(Icons.warning,color: Colors.white,),
+                                      SizedBox(width: 15,),
+                                      Text("Inputs are empty!")
+                                    ],
+                                  ),
+                                  margin: EdgeInsets.only(
+                                    bottom: MediaQuery.of(context).size.height - 200,
+                                    left: 10,
+                                    right: 10,
+                                  ),
+                                  behavior: SnackBarBehavior.floating,
+                                  dismissDirection: DismissDirection.up,
+                                  backgroundColor: Color(0xFFff0030),
+                                  showCloseIcon: true,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                              );
+                            }
+
+                          });
+                        },
+                        child: Text("Add Contact",style: TextStyle(fontSize: 15),),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFff0030),
+                          foregroundColor: Colors.white,
+                          minimumSize: Size(300, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12), ),
+                          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40), // Padding inside the button
+                        ),
+                      )
+
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            showContact(contacts: contacts)
+          ],
+        ),
+      ),
+    );
+  }
+}
